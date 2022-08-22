c31099fd739e5df441956f574705bff2050e9a3d
Artabotz <111733753+Artabotz@users.noreply.github.com>
2022-08-22 17:38:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ global.APIKeys = {
 }
 
 // NAMBAH BOLEH HAPUS JANGAN -_-
-global.owner = ['6281511480762']
-global.premium = ['621511480762']
-global.packname = 'AzzBot√'
-global.author = 'AzzBot-MD'
+global.owner = ['6287752078682']
+global.premium = ['6287752078682']
+global.packname = 'JonsBot√'
+global.author = 'JonsBot-MD'
 global.sessionName = 'Xavior'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
